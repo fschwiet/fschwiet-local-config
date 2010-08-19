f5b7d4681f0bdd3be8356fd1da890434c76d0b30
= <=>
2010-08-18 22:08:24 -0700

    removing files

--- diffmerge.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-echo Launching DiffMerge.exe $1 $2
-"C:/Program Files (x86)/SourceGear/DiffMerge/DiffMerge.exe" $1 $2
--- notepad.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo launching Programmers Notepad, parameter: $1 ignoring: $2 $3
-notepad $1 | cat
-
