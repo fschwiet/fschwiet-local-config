22061ca95a32ad0cc83ee0230f1830d9e2729b2c
fschwiet <fschwiet@gmail.com>
2012-07-01 22:44:50 -0700

    copy sublime config

---
+++ sublime/sublime-settings.ps1
@@ -0,0 +1,47 @@
+
+param(
+    $configPath = "C:\Users\" + $env:username + "\AppData\Roaming\Sublime Text 2\Packages\" + $env:username,
+    [switch] $updateGitRepo = $false,
+    $filesToExclude = @("Package Control.sublime-settings"),
+    $packagePath = ".\packages"
+    )
+
+$sourcePath = (resolve-path ".")
+$targetPath = $configPath
+
+$sourcePackagePath = (resolve-path ".\packages")
+$targetPackagePath = 'C:\Program Files\Sublime Text 2\Pristine Packages'
+
+if ($updateGitRepo) {
+    $sourcePath,$targetPath = $targetPath,$sourcePath
+    $sourcePackagePath,$targetPackagePath = $targetPackagePath,$sourcePackagePath
+}
+
+function attemptCopy($file, $t) {
+
+    $isExcluded = $filesToExclude | ? { $file.fullname.tolower().endswith($_.tolower())}
+
+    if (-not $isExcluded) {
+        ".. including $($file.name)" | write-host
+        cp $file.fullname $t
+    } else {
+        ".. skipping $($file.name)" | write-host
+    }
+}
+
+"copying from $sourcePath to $targetPath"
+
+gci $sourcePath "*.sublime-settings" | % { attemptCopy $_ $targetPath }
+
+if (-not $updateGitRepo) {
+    "copying from $sourcePackagePath to $targetPackagePath"
+
+    gci $sourcePackagePath "*.sublime-package" | % { attemptCopy $_ $targetPackagePath }
+}
+
+if (-not $updateGitRepo) {
+    "if you meant to write to the current machine's config, use -updateGitRepo after reverting what you just imported from config" | write-host -fore yellow
+} else {
+    "where any module configs copied?  maybe they should be skipped"
+}
+
