cc4b5e98cce160468b78148d1da509aa5efe8d7c
fschwiet <fschwiet@gmail.com>
2012-11-12 21:13:45 -0800

    update gitfirst.ps1

--- gitfirst.ps1
+++ gitfirst.ps1
@@ -1,9 +1,4 @@
 
-git config --global --add diff.tool diffmerge
-git config --global --add difftool.diffmerge.cmd 'diffmerge $LOCAL $REMOTE'
-git config --global --add merge.tool diffmerge
-git config --global --add mergetool.diffmerge.cmd 'diffmerge --merge --result=$MERGED $LOCAL $BASE $REMOTE'
-git config --global --add mergetool.diffmerge.trustexitcode false
 git config --global --add user.name fschwiet
 git config --global --add user.email fschwiet@gmail.com
 
